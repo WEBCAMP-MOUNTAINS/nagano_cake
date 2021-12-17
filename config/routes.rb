7611076c8d826e54f140ba68871a56ab3c05318e
“shun340” <92897220+shun340@users.noreply.github.com>
2021-12-17 09:27:22 +0000

    Fix Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,31 @@ devise_for :customers,skip: [:passwords,], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
   root to: 'public/homes#top'
   get 'public/homes/about' => 'homes#about', as: 'about'
-  resources :public/items, only: [:index, :show]
-  resources :public/customers, only: [:show, :edit, :update, :unsubscribe, :withbraw]
-  resources :public/cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
-  resources :public/orders, only: [:new, :confirm, :complete, :create, :index, show]
-  resources :public/addresses, only: [:index, :edit, :create, :update, :destroy]
+  
+  namespace :public do
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withbraw]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+  
+  
 
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   root to: 'admin/homes#top'
-  resources :admin/items, only: [:index, :new, :create, :show, :edit, :update]
-  resources :admin/genres, only: [:index, :create, :edit, :update]
-  resources :admin/customers, only: [:index, :show, :edit, :update]
-  resources :admin/orders, only: [:show, :update] do
-    resources :admin/order_details, only: [:update]
+  
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
   end
 end
