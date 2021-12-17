405dc38dd0c515f5ec33849efc260c3be1bd79fe
toru0119 <92391917+toru0119@users.noreply.github.com>
2021-12-17 08:38:39 +0000

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,22 @@ devise_for :customers,skip: [:passwords,], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+  root to: 'public/homes#top'
+  get 'public/homes/about' => 'homes#about', as: 'about'
+  resources :public/items, only: [:index, :show]
+  resources :public/customers, only: [:show, :edit, :update, :unsubscribe, :withbraw]
+  resources :public/cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+  resources :public/orders, only: [:new, :confirm, :complete, :create, :index, show]
+  resources :public/addresses, only: [:index, :edit, :create, :update, :destroy]
 
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'admin/homes#top'
+  resources :admin/items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :admin/genres, only: [:index, :create, :edit, :update]
+  resources :admin/customers, only: [:index, :show, :edit, :update]
+  resources :admin/orders, only: [:show, :update] do
+    resources :admin/order_details, only: [:update]
+  end
 end
