3b245ecb091accf01d56b9eb277ff35f171e0f97
kchishima <93571441+kchishima@users.noreply.github.com>
2021-12-19 07:39:59 +0000

    addcontrollers

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ devise_for :customers,skip: [:passwords,], controllers: {
 }
 
   root to: 'public/homes#top'
-  get 'public/homes/about' => 'homes#about', as: 'about'
-  
+  get '/about' => 'public/homes#about'
+
   namespace :public do
     resources :items, only: [:index, :show]
     resources :customers, only: [:show, :edit, :update, :unsubscribe, :withbraw]
@@ -14,20 +14,18 @@ devise_for :customers,skip: [:passwords,], controllers: {
     resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
-  
-  
 
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   root to: 'admin/homes#top'
-  
+
   namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update] do
-      resources :order_details, only: [:update]
+    resources :order_details, only: [:update]
     end
   end
 end
