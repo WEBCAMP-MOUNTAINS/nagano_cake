c4bd09c6a90785de1aba6c7e044b6d51fa3cd6e1
kchishima <93571441+kchishima@users.noreply.github.com>
2021-12-21 10:06:08 +0000

    Add customer show.hetml.erb

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,16 +2,6 @@ class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
   def show
     @customer = Customer.find(params[:id])
-    @last_name = current_customer.last_name
-    @first_name = current_customer.first_name 
-    @full_name = 
-    @last_name_kana = current_customer.last_name_kana
-    @first_name_kana = current_customer.first_name_kana
-    @postal_code = current_customer.postal_code
-    @address = current_customer.address
-    @phone_number = current_customer.phone_number
-    @encrypted_password = current_customer.encrypted_password
-    @email = current_customer.email
   end
   def edit
   end
