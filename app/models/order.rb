d9a8636c88c8d932b4deffe9770aba364e4e495b
shot-a07 <92033602+shot-a07@users.noreply.github.com>
2021-12-23 12:42:02 +0000

    Add Order View

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,7 @@ class Order < ApplicationRecord
   validates :payment_method, presence: true
   validates :order_status, presence: true
   
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_deposit: 0, payment_confirmation: 1, production: 2, preparing_ship: 3, sent: 4 }
+  
 end
