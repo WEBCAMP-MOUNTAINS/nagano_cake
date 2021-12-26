2e469ed96a74c4b4f5effeafbc85b7ea165774d7
kchishima <93571441+kchishima@users.noreply.github.com>
2021-12-26 12:05:24 +0000

    Merge Befor

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,17 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name_kana, presence: true
+  # validates :first_name_kana, presence: true
+  # validates :email, presence: true
+  # validates :encrypted_password, presence: true
+  # validates :postal_code, presence: true
+  # validates :address, presence: true
+  # validates :phone_number, presence: true
+  
+  
 
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
